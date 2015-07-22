1d241d6a1627333d189dc7d760cd1ae245c057dd
Jaime Emesto Forero Romero <j.e.forero.romero@gmail.com>
2015-07-22 10:59:41 +0100

    basic setup to select tiles

---
+++ py/quicksurvey/nextfields/select.py
@@ -0,0 +1,26 @@
+"""
+Functions to make the selection of the next field to be observed.
+Most of this depends on dates, observatory position and moon.
+"""
+
+def all_available_files(directory="./", condition="*"):
+    """
+    Returns the list of all files in a directory matching some string 
+    condition.
+    
+    Args:
+        directory (stri): Parameter. Path to the directory to search. 
+            Defaults to "./"
+        condition (str): Parameter. Condition to search in the directory.
+            Defaults to "*"
+
+    Returns:
+        filename_list: list of filenames in 'directory' matching 'condition'
+    """
+    import glob
+
+    filename_list = glob.glob(directory + '/' + condition)
+    
+    return filename_list
+
+    
